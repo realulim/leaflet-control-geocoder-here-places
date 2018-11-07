6e5892c7f681bb21a7b623519ee8eecb68364d88
Ulrich Mayring <ulim@mayring.de>
2018-11-07 12:10:45 +0100

    refactor to HerePlaces

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p dist
-browserify src/index.js -t es3ify -t browserify-shim -o dist/Control.Geocoder.Dawa.js
+browserify src/index.js -t es3ify -t browserify-shim -o dist/Control.Geocoder.HerePlaces.js
 cp -r node_modules/leaflet-control-geocoder/dist .
