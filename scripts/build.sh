d1d4bd9d63eced1a326a4173d4dde62b1586cf72
Ulrich Mayring <ulim@mayring.de>
2018-11-15 11:54:29 +0100

    fixed path to browserify

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-browserify src/index.js -t [ babelify --presets [@babel/preset-env] ] -t es3ify -t browserify-shim -o dist/Control.Geocoder.HerePlaces.js
+./node_modules/browserify/bin/cmd.js src/index.js -t [ babelify --presets [@babel/preset-env] ] -t es3ify -t browserify-shim -o dist/Control.Geocoder.HerePlaces.js
