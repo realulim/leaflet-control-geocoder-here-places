7d77cbe1cb97e1e92f5a74214c765fb1687503fe
Ulrich Mayring <ulim@mayring.de>
2018-11-15 12:47:22 +0100

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p dist
-./node_modules/browserify/bin/cmd.js src/index.js -t [ babelify --presets [@babel/preset-env] ] -t es3ify -t browserify-shim -o dist/Control.Geocoder.HerePlaces.js
+browserify src/index.js -t [ babelify --presets [@babel/preset-env] ] -t es3ify -t browserify-shim -o dist/Control.Geocoder.HerePlaces.js
